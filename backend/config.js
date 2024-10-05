8bc6885bfe2037b8c9e6a9a68af623fca060413e
ThulaniDissanayake <IT22157232@my.sliit.lk>
2024-10-05 12:26:05 +0530

    firstTransport

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT =5555;
+
+export const mongoDBURL = 'mongodb+srv://root:IEaKA4zUxOAymfLA@books-store-mern.tbcrd4f.mongodb.net/?retryWrites=true&w=majority&appName=Books-Store-MERN';
